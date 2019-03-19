9b1a4f8e99ea8a1ea0d0a48dc2f62682418b2c94
Vadim Babajanyan <akeb@akeb.ru>
2019-03-19 14:36:01 +0300

    TEST

--- tests/memcachedTest.php
+++ tests/memcachedTest.php
@@ -1,22 +1,23 @@
 <?php
 
-if (extension_loaded('memcache')) {
-class memcacheTest extends PHPUnit\Framework\TestCase {
+if (extension_loaded('memcached')) {
+class memcachedTest extends PHPUnit\Framework\TestCase {
 	public function testMemcacheFunctions() {
 		var_export(get_loaded_extensions());
 
-		$this->assertTrue(class_exists('Memcache') ? true : false , 'Class Error');
+		$this->assertTrue(class_exists('Memcached') ? true : false , 'Class Error');
 		$this->assertTrue(function_exists('memcache_get') ? true : false, 'Function Error');
 
-		$memcache_obj = new Memcache;
+		$memcache_obj = new Memcached;
 		if (@$memcache_obj->addServer('localhost', 11211)) {
 			$rand = rand(0, mt_getrandmax());
-			$memcache_obj->set('TestKey', $rand, MEMCACHE_COMPRESSED, 10);
+			$memcache_obj->set('TestKey', $rand, 10);
 			$cache_rand = $memcache_obj->get('TestKey');
 			$this->assertEquals($rand, $cache_rand, 'set get Error');
 		} else {
 			var_dump('Error Connect');
 		}
 	}
+
 }
 }
