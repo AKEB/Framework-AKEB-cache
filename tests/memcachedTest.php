622b3f96622ee7da80d143d76697e6e50d60ed7f
Vadim Babajanyan <akeb@akeb.ru>
2019-03-19 16:22:15 +0300

    MemcacheTests

--- tests/memcachedTest.php
+++ tests/memcachedTest.php
@@ -13,17 +13,18 @@ class memcachedTest extends PHPUnit\Framework\TestCase {
 	}
 
 	public function testMemcacheFunctions() {
+		$memcache_obj1 = new newMemcache();
+		$status = $memcache_obj1->connect('localhost', 11288);
+		$this->assertFalse($status, 'Connect status error');
+
 		$memcache_obj = new newMemcache();
-		$status = $memcache_obj->connect('localhost', 11212);
+		$status = $memcache_obj->connect('localhost', 11211);
 		$this->assertTrue($status, 'Connect error');
-		if ($status) {
-			$rand = rand(0, mt_getrandmax());
-			$memcache_obj->set('TestKey', $rand, 10);
-			$cache_rand = $memcache_obj->get('TestKey');
-			$this->assertEquals($rand, $cache_rand, 'set get Error');
-		} else {
-			var_dump('Error Connect');
-		}
+		$rand = rand(0, mt_getrandmax());
+		$memcache_obj->set('TestKey', $rand, 10);
+		$cache_rand = $memcache_obj->get('TestKey');
+		$this->assertEquals($rand, $cache_rand, 'set get Error');
+
 	}
 
 }
