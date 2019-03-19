c5e11f3ea15aceafb146546d8b4dc528710814bd
Vadim Babajanyan <akeb@akeb.ru>
2019-03-19 13:28:09 +0300

    TEST

--- tests/memcacheTest.php
+++ tests/memcacheTest.php
@@ -2,16 +2,17 @@
 
 class memcacheTest extends PHPUnit\Framework\TestCase {
 	public function testMemcacheFunctions() {
-		phpinfo();
-		$this->assertTrue(class_exists('Memcache') ? true : false);
-		$this->assertTrue(function_exists('memcache_get') ? true : false);
+		var_dump(get_loaded_extensions());
+
+		$this->assertTrue(class_exists('Memcache') ? true : false , 'Class Error');
+		$this->assertTrue(function_exists('memcache_get') ? true : false, 'Function Error');
 
 		$memcache_obj = new Memcache;
 		if (@$memcache_obj->connect('localhost', 11211)) {
 			$rand = rand(0, mt_getrandmax());
 			$memcache_obj->set('TestKey', $rand, MEMCACHE_COMPRESSED, 10);
 			$cache_rand = $memcache_obj->get('TestKey');
-			$this->assertEquals($rand, $cache_rand);
+			$this->assertEquals($rand, $cache_rand, 'set get Error');
 		} else {
 			var_dump('Error Connect');
 		}
