69f0b47242c4781bd7d8860acfb732dd3a06cc4b
Vadim Babajanyan <akeb@akeb.ru>
2019-03-20 19:16:11 +0300

    if (isset($_REQUEST['no_cache']) && $_REQUEST['no_cache']) return false;

--- src/Cache_Memcache.php
+++ src/Cache_Memcache.php
@@ -28,7 +28,7 @@ class Cache_Memcache {
 	public function isValid() {
 		if (!$this->mcServer() || !$this->cacheId()) return false;
 		if (defined('NO_CACHE') && NO_CACHE) return false;
-		if ($_REQUEST['no_cache']) return false;
+		if (isset($_REQUEST['no_cache']) && $_REQUEST['no_cache']) return false;
 		$ttlInfo = $this->mcServer()->get($this->cacheId().".ttl");
 		return $ttlInfo !== false ? ((int)$ttlInfo["createTime"] + (int)$ttlInfo["ttl"]) > time() : false;
 	}
